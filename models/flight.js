ef0d6344dae8f67eb37423fefa7bfbdbda043aaf
bcbell <coleman.britt@gmail.com>
2020-12-04 01:37:43 -0500

    Update: Flight information properly positioned

--- models/flight.js
+++ models/flight.js
@@ -3,14 +3,14 @@ const Schema= mongoose.Schema
 
 const oneYear= 365*24*60*60000
 const oneYearLater= function(){
-    return new Date (getDay() + oneYear)
+    return new Date (Date.now() + oneYear)
 }
 
 const flightSchema = new Schema({
     airline: {type: String, enum:['American', 'Southwest', 'United', 'Delta', 'jetBlue', 'Frontier', 'Spirit'], default:''},
     airport: {type: String, enum:['AUS', 'DFW', 'DEN', 'LAX', 'SAN','ATL', 'JFK'], default: 'DEN'},
     flightNo:{type: Number, required: [true,'Please enter your flight number'], min:10, max:9999, default: ''},
-    departs: {type:Date, default: oneYearLater}
+    departs: {type:Date, max: oneYearLater, default: new Date(Date.now()+365*24*60*60*1000)}
 }) 
  
 
