728f45e7a174466e18689b8e4682f86060c08f21
bcbell <coleman.britt@gmail.com>
2020-12-06 23:59:47 -0500

    Update: Create function created

--- controller/destinations.js
+++ controller/destinations.js
@@ -1,11 +1,17 @@
 const Destination = require('../models/destination')
 
 module.exports={
-    new : newDestination
+    new : newDestination,
+    create
 }
 
 function newDestination(req,res){
     Destination.find({}, function(err, destinations){
     res.render('destinations/new', {title:'Flight Destination', destinations})
 })
+}
+function create(req, res){
+    Destination.create(req.body, function(err, destination){
+        res.redirect('/destinations/new')
+   }) 
 }
--- routes/destinations.js
+++ routes/destinations.js
@@ -3,5 +3,6 @@ const router =express.Router()
 const destCtrl = require('../controller/destinations')
 
 router.get('/destinations/new', destCtrl.new)
+router.post('/destinations/new', destCtrl.create)
 
 module.exports= router
