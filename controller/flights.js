99d5436a0b93264dfdb6d0c6af713e5f376b094e
bcbell <coleman.britt@gmail.com>
2020-12-07 02:57:58 -0500

    Update: Able to access individual tickets, but will work on rendering destinations on ticket pages

--- controller/flights.js
+++ controller/flights.js
@@ -1,5 +1,4 @@
 const Flight = require('../models/flight')
-const Destination =require('../models/destination')
 
 module.exports= {
     index, 
@@ -21,34 +20,15 @@ function newSearch(req, res){
 }
 function create (req, res, next){
     Flight.create(req.body)
-    if(req.body.departs){
-         req.body.departs > Date.now()+ 365*24*60*60000 
+    if(req.body.departs) req.body.departs > Date.now()+ 365*24*60*60000             
+    res.redirect('/flights')  
 }
-        const flight = new Flight (req.body)
-        flight.save(function(err){
-            if (err)
-            console.log(err)
-            return res.render ('flights/new', {err:err})
-        },
-        console.log(flight),
-        res.redirect(`/flights/${flight._id}`),             
-        )}
-
 
 function show(req, res){
-    Flight.findById(req.params.id)
-    .populate('destinations').exec(function(err, flight){
-        Destination.find({_id:{$nin:flight.destinations}}),
-        function(err, destinations){
-           res.render('flights/show', {title: `Flight Details`, flights: flight},
-           )}
+    Flight.findById(req.params.id, function(err, flight){
+      res.render('flights/show', {title: `Flight Details`, flights: flight})  
     })
+    
 }
-    
-    
-       
-   
-    
-
 
 
