bb2f1eedb560342f663410a8e7e3c7a3f23c4f84
bcbell <coleman.britt@gmail.com>
2020-12-09 12:21:21 -0500

    Update: Updated button

--- server.js
+++ server.js
@@ -26,9 +26,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(methodOverride('_method'));
 
 app.use('/', indexRouter);
-app.use('/flights', flightsRouter);
 app.use('/', destinationsRouter)
 app.use('/', ticketsRouter)
+app.use('/flights', flightsRouter);
+
+
 
 
 // catch 404 and forward to error handler
